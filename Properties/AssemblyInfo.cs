fae48ecd75ef7f1bbb20c6e27bf605956d4c516d
Paul Rayman <pr@patagames.com>
2018-05-02 15:56:19 +1000

    - release 3.26.2704

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -11,8 +11,8 @@ using System.Windows;
 //      Revision -.Net framework
 
 //---------------------------- WPF Viewer -------------------------------------------------
-[assembly: AssemblyInformationalVersion("3.25.2704")]  //Should be equal to save property of Patagames.Pdf assembly
-[assembly: AssemblyVersion("3.14.9." +
+[assembly: AssemblyInformationalVersion("3.26.2704")]  //Should be equal to save property of Patagames.Pdf assembly
+[assembly: AssemblyVersion("3.14.10." +
 #if DOTNET20
 "20"
 #elif DOTNET30
