be42bc076be57007c63fff65b3bb2528c7f5982a
Paul Rayman <pr@patagames.com>
2020-09-29 15:50:10 +1000

    -release

--- PdfViewer.cs
+++ PdfViewer.cs
@@ -272,7 +272,7 @@ namespace Patagames.Pdf.Net.Controls.Wpf
         /// Raises the <see cref="BeforeDocumentChanged"/> event.
         /// </summary>
         /// <param name="e">An System.EventArgs that contains the event data.</param>
-        /// <returns>True if changing should be canceled, False otherwise</returns>
+        /// <returns>True if the change should be canceled; otherwise - False</returns>
         protected virtual bool OnBeforeDocumentChanged(DocumentClosingEventArgs e)
         {
             if (BeforeDocumentChanged != null)
@@ -290,12 +290,12 @@ namespace Patagames.Pdf.Net.Controls.Wpf
                 DocumentLoaded(this, e);
         }
 
-        /// <summary>
-        /// Raises the <see cref="DocumentClosing"/> event.
-        /// </summary>
-        /// <param name="e">An System.EventArgs that contains the event data.</param>
-        /// <returns>True if closing should be canceled, False otherwise</returns>
-        protected virtual bool OnDocumentClosing(DocumentClosingEventArgs e)
+		/// <summary>
+		/// Raises the <see cref="DocumentClosing"/> event.
+		/// </summary>
+		/// <param name="e">An System.EventArgs that contains the event data.</param>
+		/// <returns>True if the close should be canceled; otherwise - False</returns>
+		protected virtual bool OnDocumentClosing(DocumentClosingEventArgs e)
         {
             if (DocumentClosing != null)
                 DocumentClosing(this, e);
--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Windows;
 //      Revision -.Net framework
 
 //---------------------------- WPF Viewer -------------------------------------------------
-[assembly: AssemblyInformationalVersion("4.33.2704")]  //Should be equal to the same property of Patagames.Pdf assembly
+[assembly: AssemblyInformationalVersion("4.36.2704")]  //Should be equal to the same property of Patagames.Pdf assembly
 [assembly: AssemblyVersion("4.10.13." +
 #if DOTNET20
 "20"
